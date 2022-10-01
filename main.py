8da63464255e59dc05aafc06786d85453445eeb1
Yauhen Yavorski <yauheny@uia.no>
2022-10-01 12:29:18 +0200

    refactor: move calculate angle function to separate file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import cv2  # type: ignore
 import mediapipe as mp  # type: ignore
 import numpy as np
 
+from functions.calculate_angle_between_points import calculate_angle_between_points
+
 # Gives us all the drawing utilities. Going to be used to visualize the poses
 mp_drawing = mp.solutions.drawing_utils
 
@@ -23,24 +25,6 @@ def make_detections(pose, frame):
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
     return image, results
-
-
-def calculate_angle_between_points(pointA, pointB, pointC):
-    # convert the point to numpy arrays
-    pointA = np.array(pointA)  # first point
-    pointB = np.array(pointB)  # mid point
-    pointC = np.array(pointC)  # last point
-
-    # calculate the angle using trigonometry and convert it to degrees
-    angle_in_radians = np.arctan2(
-        pointC[1] - pointB[1], pointC[0] - pointB[0]
-    ) - np.arctan2(pointA[1] - pointB[1], pointA[0] - pointB[0])
-    angle_in_degrees = np.abs(angle_in_radians * 180.0 / np.pi)
-
-    if angle_in_degrees > 180.0:
-        angle_in_degrees = 360 - angle_in_degrees
-
-    return angle_in_degrees
 
 
 if __name__ == "__main__":
